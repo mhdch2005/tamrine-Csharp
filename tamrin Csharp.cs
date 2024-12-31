a9606ec190683befd8fcb72db124c50862ec9156
mhdch2005 <138624384+mhdch2005@users.noreply.github.com>
2024-12-31 23:33:48 +0330

    تمرین سی شارپ

---
+++ tamrin Csharp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("عدد را وارد کنید: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine($"عدد {n} جمله در دنباله فیبوناچی: {Fibonacci(n)}");
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+            return n;
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+}
